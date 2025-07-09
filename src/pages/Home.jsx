7023d48d180f0c6f5d90819f29d091255d13b014
sofiane <sofianehot01@gmail.com>
2025-07-09 18:32:46 +0100

    fixing home layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,29 @@
 import { Link } from 'react-router-dom';
 const Home = () => {
   return (
-    <div className='p-4 flex flex-col h-full w-full justify-center'>
+    <div className="p-4 flex flex-col justify-center items-center h-screen bg-gray-200  w-screen ">
 
-        <nav className="space-y-5 flex-1">
-                    <Link to="/register" className="flex bg-[#0B2027] items-center hover:text-gray-300">
-                      <span>Sign up</span>
-                    </Link>
-        </nav>
-        <nav className="space-y-5 flex-1">
-                    <Link to="/login" className="flex bg-[#0B2027] items-center hover:text-gray-300">
-                      <span>Login</span>
-                    </Link>
-        </nav>
+        <header className="bg-[#F5F5F5] flex absolute top-0 justify-between items-center h-[70px] w-screen px-5">
+            <h2 className="font-bold text-2xl text-[#0B2027] font-[Florisha]">Trackmint</h2>
+        
+            <nav className="flex ">
+                <Link
+                    to="/login"
+                    className="flex items-center justify-center text-white rounded-xl bg-[#0B2027] hover:bg-[#173f4f] py-2 px-6 mx-3"
+                    >
+                    <span>Login</span>
+                </Link>
+                <Link
+                    to="/register"
+                    className="flex items-center  justify-center text-black rounded-xl border border-2 border-[#0B2027] hover:border-[#173f4f] py-2 px-4"
+                    >
+                    <span>Sign up</span>
+                </Link>
+            </nav>
+        </header>
 
     </div>
+
   );
 };
 
