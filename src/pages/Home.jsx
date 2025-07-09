9351d3d4661f8ece9b4a6985fdcd20ee886d64e4
sofiane <sofianehot01@gmail.com>
2025-07-09 13:41:23 +0100

    adding bg to links

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,15 @@
 import { Link } from 'react-router-dom';
 const Home = () => {
   return (
-    <div className='p-4 flex flex-col h-full'>
+    <div className='p-4 flex flex-col h-full w-full justify-center'>
 
         <nav className="space-y-5 flex-1">
-                    <Link to="/register" className="flex items-center hover:text-gray-300">
+                    <Link to="/register" className="flex bg-[#0B2027] items-center hover:text-gray-300">
                       <span>Sign up</span>
                     </Link>
         </nav>
         <nav className="space-y-5 flex-1">
-                    <Link to="/login" className="flex items-center hover:text-gray-300">
+                    <Link to="/login" className="flex bg-[#0B2027] items-center hover:text-gray-300">
                       <span>Login</span>
                     </Link>
         </nav>
