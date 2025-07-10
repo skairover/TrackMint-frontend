cee84da63e3a7685cf5d8d0dc0012fe787bbf9f4
sofiane <sofianehot01@gmail.com>
2025-07-10 14:47:12 +0100

    fixing expenses service

--- src/services/expenseService.js
+++ src/services/expenseService.js
@@ -1,31 +1,5 @@
 import api from './api';
 
-export const deleteExpense = (id) => {
-  return api.delete(`/api/expenses/${id}`);
-};
-
-
-
-// Automatically attach JWT from localStorage
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
-
-// Get all expenses
-export const getExpenses = async () => {
-  const res = await api.get('/expenses');
-  return res.data;
-};
-
-// Add a new expense
-export const createExpense = async (expense) => {
-  const res = await api.post('/expenses', expense);
-  return res.data;
-};
-
-// Delete an expense by ID
-
+export const getExpenses = () => api.get('/api/expenses');
+export const createExpense = (data) => api.post('/api/expenses', data);
+export const deleteExpense = (id) => api.delete(`/api/expenses/${id}`);
