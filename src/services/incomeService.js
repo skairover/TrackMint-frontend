b97d9b23a0d4d4727ee0fc64a87ad09ac11197a0
sofiane <sofianehot01@gmail.com>
2025-07-10 14:36:57 +0100

    changing income services

--- src/services/incomeService.js
+++ src/services/incomeService.js
@@ -1,5 +1,31 @@
 import api from './api';
 
-export const getIncomes = () => api.get('/api/incomes');
-export const createIncome = (data) => api.post('/api/incomes', data);
-export const deleteIncome = (id) => api.delete(`/api/incomes/${id}`);
+export const deleteIncome = (id) => {
+  return api.delete(`/api/incomes/${id}`);
+};
+
+
+
+// Automatically attach JWT from localStorage
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+// Get all expenses
+export const getIncomes = async () => {
+  const res = await api.get('/incomes');
+  return res.data;
+};
+
+// Add a new expense
+export const createIncome = async (income) => {
+  const res = await api.post('/incomes', income);
+  return res.data;
+};
+
+// Delete an expense by ID
+
