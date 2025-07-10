1168ef4271c717615af6b3fcd22494dac9b5b042
sofiane <sofianehot01@gmail.com>
2025-07-10 14:59:02 +0100

    something

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,9 @@
 // services/api.js
 import axios from 'axios';
 
-const baseURL = import.meta.env.VITE_API_BASE_URL;
 
-const api = axios.create({ baseURL });
+
+const api = axios.create({ baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000', });
 
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('token');
