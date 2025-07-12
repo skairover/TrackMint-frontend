c07d3689227a94fd40d0053600902066449fbab1
sofiane <sofianehot01@gmail.com>
2025-07-12 16:57:57 +0100

    something

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,10 @@ import Expenses from "./pages/Expenses";
 import Incomes from "./pages/Incomes";
 import Settings from "./pages/Settings";
 import { Toaster } from 'react-hot-toast';
-import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 function App() {
   return (
-    <HelmetProvider>
-      <Helmet>
-        <title>TrackMint – Smart Expense Tracking</title>
-        <meta name="description" content="TrackMint helps you manage income and expenses efficiently with a clean dashboard and modern UI." />
-        <meta name="keywords" content="expense tracker, budget app, money management, TrackMint" />
-        <meta name="author" content="Sofiane" />
-        <meta property="og:title" content="TrackMint" />
-        <meta property="og:description" content="Smart Expense Tracking App" />
-        <meta property="og:image" content="/trackmint-preview.png" />
-      </Helmet>
-
+  
       <BrowserRouter>
         <Toaster position="top-center" />
         <Routes>
@@ -36,7 +25,7 @@ function App() {
           <Route path="/settings" element={<Settings />} />
         </Routes>
       </BrowserRouter>
-    </HelmetProvider>
+    
   );
 }
 
