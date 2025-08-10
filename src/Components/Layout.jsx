468866a80d0407ec512ff66234534f0fc6191d8b
sofiane <sofianehot01@gmail.com>
2025-08-11 00:05:10 +0100

    fixing overflow issues

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -7,15 +7,13 @@ function Layout({ title, children }) {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
-    <div className="flex min-h-screen w-screen overflow-hidden">
-      {/* Sidebar collapses into drawer on mobile */}
+    <div className="flex h-full w-screen overflow-x-hidden">
       <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
       
-      <div className="flex-1 flex flex-col text-black h-full">
+      <div className="flex-1 flex flex-col text-black h-screen">
         <Header title={title} setSidebarOpen={setSidebarOpen} />
         
-        {/* Add padding so mobile doesn’t feel cramped */}
-        <main className="flex-1 bg-gray-200 dark:bg-[#121212] overflow-y-auto p-4 sm:p-6">
+        <main className="flex-1 bg-gray-200 dark:bg-[#121212] overflow-x-scroll h-full p-4 sm:p-6">
           {children}
         </main>
       </div>
